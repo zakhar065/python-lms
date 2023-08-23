c0d1158abf40b1c680dcb63451e2a3531bcd086f
zakhar065 <115736138+zakhar065@users.noreply.github.com>
2023-08-23 12:32:20 +0300

    Update 11 Lesson.Task №1-Доработаное.py

--- 11 Lesson.Task №1-Доработаное.py
+++ 11 Lesson.Task №1-Доработаное.py
@@ -1,17 +1,19 @@
 def factorial(n):
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         result *= i
     return result
 
 def factorial_list(n):
-    factorials = [factorial(i) for i in range(n, 0, -1)]
+    factorial_n = factorial(n)
+    factorials = []
+    for i in range(factorial_n, 0, -1):
+        factorials.append(factorial(i))
     return factorials
 
 number = int(input("Введите число: "))
 result = factorial_list(number)
 print(result)
-
-print("консоль закроется через 10 секунд...")
+print("консоль закроется через 60 секунд...")
 import time
-time.sleep(10)
+time.sleep(60)
