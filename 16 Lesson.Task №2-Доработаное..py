4f36339770414116fce1780aa2dfe3ddc4b535ef
zakhar065 <115736138+zakhar065@users.noreply.github.com>
2023-08-23 12:42:41 +0300

    Add files via upload

---
+++ 16 Lesson.Task №2-Доработаное..py
@@ -0,0 +1,46 @@
+class Turtle:    
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s    
+
+    def go_up(self):
+        self.y += self.s
+
+    def go_down(self):
+        self.y -= self.s    
+
+    def go_left(self):
+        self.x -= self.s
+
+    def go_right(self):
+        self.x += self.s   
+
+    def evolve(self):
+        self.s += 1    
+
+    def degrade(self):
+        if self.s > 1:
+            self.s -= 1
+        else:
+            raise ValueError("Невозможно уменьшить s")    
+
+    def count_moves(self, x2, y2):
+        dx = abs(x2 - self.x)
+        dy = abs(y2 - self.y)
+        return max(dx, dy) // self.s
+
+turtle = Turtle(0, 0, 1)
+print("Начальная позиция черепашки:", turtle.x, turtle.y)
+turtle.evolve()
+print("Количество клеточек на каждом ходу после эволюции:", turtle.s)
+turtle.go_up()
+print("Новая позиция после хода вверх:", turtle.x, turtle.y)
+turtle.degrade()
+print("Количество клеточек на каждом ходу после обратного развития:", turtle.s)
+moves = turtle.count_moves(3, 4)
+print("Минимальное количество действий до (3, 4):", moves)
+
+print("консоль закроется через 60 секунд...")
+import time
+time.sleep(60)
