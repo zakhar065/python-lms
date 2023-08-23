a1cad694f91ee94c48dac33c7cf738ce293a2d5c
zakhar065 <115736138+zakhar065@users.noreply.github.com>
2023-08-23 12:26:31 +0300

    Update 5 Lesson.Task №2 -Доработаное.py

--- 5 Lesson.Task №2 -Доработаное.py
+++ 5 Lesson.Task №2 -Доработаное.py
@@ -1,22 +1,26 @@
 word = input("Введите слово: ")
 vowels = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
 consonants = 0
+total_vowels = 0
 
 for letter in word.lower():
     if letter in vowels:
         vowels[letter] += 1
+        total_vowels += 1
     elif letter.isalpha():
         consonants += 1
 
-if all(value == 0 for value in vowels.values()) or consonants == 0:
+if total_vowels == 0 or consonants == 0:
     print(False)
 else:
-    print("Количество гласных:")
+    print("Количество гласных:", total_vowels)
+    print("Количество каждой гласной:")
     for vowel, count in vowels.items():
         if count > 0:
             print(vowel + ":", count)
     print("Количество согласных:", consonants)
 
-print("консоль закроется через 10 секунд...")
+
+print("консоль закроется через 60 секунд...")
 import time
-time.sleep(10)
+time.sleep(60)
