31ed64a7f436440407084a4adc1b26046912abeb
zakhar065 <115736138+zakhar065@users.noreply.github.com>
2023-08-23 12:33:31 +0300

    Update 15 Lesson.Task №1-Доработаное.py

--- 15 Lesson.Task №1-Доработаное.py
+++ 15 Lesson.Task №1-Доработаное.py
@@ -1,17 +1,15 @@
 class Transport:
-    def __init__(self, name, max_speed, mileage):
+    def __init__(self, name, maxspeed, mileage):
         self.name = name
-        self.max_speed = max_speed
+        self.maxspeed = maxspeed
         self.mileage = mileage
 
-class Autobus(Transport):
-    pass
+transport = Transport("Renault Logan", 180, 12)
+print("Название автомобиля:", transport.name)
+print("Скорость:", transport.maxspeed)
+print("Пробег:", transport.mileage)
 
-autobus = Autobus("Renault Logan", 180, 12)
-print("Название автомобиля:", autobus.name)
-print("Скорость:", autobus.max_speed)
-print("Пробег:", autobus.mileage)
-
-print("консоль закроется через 10 секунд...")
+print("консоль закроется через 60 секунд...")
 import time
-time.sleep(10)
+time.sleep(60)
+
